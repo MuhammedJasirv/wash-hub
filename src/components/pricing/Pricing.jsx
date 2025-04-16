fd8260bb3bdd43b52ec3d43a4555fcf5310296e3
MuhammedJasirv <muhammed.jasir@ivorytechnologies.com>
2025-04-16 19:40:06 +0530

    PricePageUpadated

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './Pricing.scss'
+import PricePoint from '../../assets/images/pricePoint.png'
 const Pricing = () => {
   return (
     <div className='pricing'>
@@ -13,6 +14,18 @@ const Pricing = () => {
         <ToggleSwitch />
       </div>
       <span>Monthly Pricing</span>
+    </div>
+    <div className="price-details">
+        <p className='price'><span>₹</span>999</p>
+        <p className='month'>/month</p>
+        <p className='description'>Running a laundry business can be complex. From managing scheduling pickups, keeping track of inventory, and handling  requestsRunning a laundry business can be complex. From managing scheduling pickups, keeping track of inventory, and handling  requestsRunning a laundry business can be complex. From managing scheduling pickups, keeping track of inventory, </p>
+        <div className='points'>
+            <div className="items"><img src={PricePoint} alt="" /> <p>Running a laundry business can be complex. From managing scheduling pickups, </p></div>
+            <div className="items"><img src={PricePoint} alt="" /> <p>Running a laundry business can be complex. From managing scheduling pickups, </p></div>
+            <div className="items"><img src={PricePoint} alt="" /> <p>Running a laundry business can be complex. From managing scheduling pickups, </p></div>
+            <div className="items"><img src={PricePoint} alt="" /> <p>Running a laundry business can be complex. From managing scheduling pickups, </p></div>
+        </div>
+        <button className="choose-plan-button">Choose Plan</button>
     </div>
     </div>
   )
