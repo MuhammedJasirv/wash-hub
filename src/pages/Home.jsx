fe4eea80f38acc15838bdf02e6ba83bf193452a3
MuhammedJasirv <muhammed.jasir@ivorytechnologies.com>
2025-05-21 18:02:05 +0530

    update responve

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,34 @@
-import React from 'react'
-import LandingPage from '../components/landingPage/LandingPage'
-import FooterLandingPage from '../components/footerLandingPage/FooterLandingPage'
-import About from '../components/about/About'
-import Service from '../components/service/Service'
-import Vision from '../components/vision/Vision'
-import HowItWorks from '../components/howItWorks/HowItWorks'
-import Pricing from '../components/pricing/Pricing'
-import Footer from '../components/footer/Footer'
-import Features from '../components/features/Features'
-import BgImage from '../assets/images/desing/bg.png';
+import React from 'react';
+import './Home.scss';
+
+import LandingPage from '../components/landingPage/LandingPage';
+import FooterLandingPage from '../components/footerLandingPage/FooterLandingPage';
+import About from '../components/about/About';
+import Service from '../components/service/Service';
+import Vision from '../components/vision/Vision';
+import HowItWorks from '../components/howItWorks/HowItWorks';
+import Pricing from '../components/pricing/Pricing';
+import Footer from '../components/footer/Footer';
+import Features from '../components/features/Features';
+
 const Home = () => {
   return (
-    <div>
-       <div
-      style={{
-        backgroundImage: `url(${BgImage})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
-    >
-        <LandingPage/>
-        <Features/>
-        <About/>
+    <div className="home-container">
+      <div className="home-hero">
+        <LandingPage />
+        <Features />
+        <About />
       </div>
-        <Service/>
-        <Vision/>
-        <HowItWorks/>
-        <Pricing/>
-        <Footer/>
-    </div>
-  )
-}
 
-export default Home
+      <div className="home-content">
+        <Service />
+        <Vision />
+        <HowItWorks />
+        <Pricing />
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
